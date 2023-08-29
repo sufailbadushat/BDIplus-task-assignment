565fa0016377828a636ec0c342f1078eac1b3858
sufailbadusha <sufailbadushat@gmail.com>
2023-08-29 13:40:27 +0530

    bug fix - add id in get all and get by id Dto

--- src/main/java/com/bdiplus/task/entity/Task.java
+++ src/main/java/com/bdiplus/task/entity/Task.java
@@ -15,6 +15,12 @@ public class Task {
     private String title;
     private String description;
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getTitle() {
         return title;
     }

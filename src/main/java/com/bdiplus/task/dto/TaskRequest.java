485e1e4b61226b870b353404ff2796196eb3addd
sufailbadusha <sufailbadushat@gmail.com>
2023-08-30 11:29:14 +0530

    controller unit test done

--- src/main/java/com/bdiplus/task/dto/TaskRequest.java
+++ src/main/java/com/bdiplus/task/dto/TaskRequest.java
@@ -3,14 +3,20 @@ package com.bdiplus.task.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Setter
 @Getter
 public class TaskRequest {
     @NotNull(message = "title cannot be null")
     private String title;
     private String description;
+    public TaskRequest() {
+
+    }
+    public TaskRequest(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 
     public String getTitle() {
         return title;
